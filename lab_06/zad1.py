946ae88589cd055090f4aff9e30ef20954f9d651
Szymon Śliwa <szymon242820@gmail.com>
2023-04-05 20:51:15 +0200

    lab06 zad1

--- lab_06/zad1.py
+++ lab_06/zad1.py
@@ -1,1 +1,27 @@
-# a gdzie jest niby ten plic zamowienia.csv ???
+# No ogólnie pliku zamówienia nie ma na tym repo, mam go bo robiłem laby z poprzedniego roku
+# EDIT: ADRIAN DAŁ INFO ŻE PLIK ZARAZ BĘDZIE
+import codecs
+from datetime import datetime
+
+lista = []
+# zapis do listy
+with codecs.open('zamowienia.csv', 'r', 'utf-8-sig') as f:
+    for line in f:
+        lista.append(line.replace('\n', '').replace('\r', '').replace(' z\x88', '').split(';'))
+
+# zamiana przecinka na kropke
+for line in lista[1:]:
+    line[len(line)-1] = line[len(line)-1].replace(',','.').replace(' ', '')
+    line[2] = datetime.strptime(line[2], '%d.%m.%Y').strftime('%Y-%m-%d')
+    
+
+pl = [x for x in lista if x[0] == 'Polska']
+de = [x for x in lista if x[0] == 'Niemcy']
+
+with open('zamowienia_polska.csv', 'w') as f:
+    for line in pl:
+        f.write(";".join(line)+'\n')
+
+with open('zamowienia_niemcy.csv', 'w') as f:
+    for line in de:
+        f.write(";".join(line)+'\n')
